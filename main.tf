07df418d1bc40dde03432ec2c7733ce56c4d1b9e
Mark Golino <markvpr@gmail.com>
2024-10-16 13:39:13 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -41,10 +41,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt_the_bucke
 resource "aws_s3_bucket_public_access_block" "block_the_bucket" {
   bucket = aws_s3_bucket.mpgbucket1968.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "policy_the_bucket" {
